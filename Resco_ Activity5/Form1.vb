538a61104f77b2706ed0b0d42cdcc883793fc3f7
STC <STC@DESKTOP-N6V91P3>
2024-02-15 14:15:05 +0800

    Add project files.

---
+++ Resco_ Activity5/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btndivide_Click(sender As Object, e As EventArgs) Handles btndivide.Click
+        Try
+            Dim a, b As Integer
+            a = txtfirstnum.Text
+            b = txtsecondnum.Text
+            lblresult.Text = a / b
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+
+        End Try
+    End Sub
+End Class
